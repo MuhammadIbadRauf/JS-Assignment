d2f5b6266f03017a0dbc9dd7141a1aec9c291458
MuhammadIbadRauf <ibadrauf123@gmail.com>
2020-06-11 23:51:51 +0500

    Javascript Assignment

---
+++ chapter3/app.js
@@ -0,0 +1,23 @@
+//__________________________________TASK : 1___________________________________
+
+var age=15;
+alert(`I am ${age} Years old`);
+
+
+//____________________________________TASK : 3______________________________
+
+
+var birthYear=2000;
+document.write("My birth year is "+birthYear);
+document.write("<br> Data type of my declared variable is number");
+
+
+//_____________________________________TASK : 4_____________________________________
+
+var name=prompt("Name: ");
+var order=prompt("Order: ");
+var quantity=+prompt("How many: ");
+document.write("<br />"+name+" ordered "+quantity+ order+ " on XYZ clothing store.");
+
+//_______________________________________END____________________________________
+
